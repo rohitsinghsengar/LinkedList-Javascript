c7deacbc7b57745c59d6faef191798210c492f14
Rohit Singh Sengar <rohitsinghsengar@gmail.com>
2015-10-28 14:48:14 +0530

    Added reverse function
    Added function to reverse the linkedlist

--- linkedlist.js
+++ linkedlist.js
@@ -4,31 +4,31 @@ function LinkedList() {
 }
 
 LinkedList.prototype = {
-    _createNode: function(val) {
-        return {
-            data: val,
-            next: null
-        }
-    },
-    push: function(val) {
-        var current,
-        newNode = this._createNode(val); 
-        if(this._head == null) {
-            this._head = newNode;
-        }
-        else {
-            current = this._head;
-            while(current.next != null) {
-                current = current.next;
-            }
-            current.next = newNode;
-        }
-        this._size++;
-    },
+	_createNode: function(val) {
+		return {
+		    data: val,
+		    next: null
+		}
+	},
+	push: function(val) {
+		var current,
+		newNode = this._createNode(val); 
+		if(this._head == null) {
+		    this._head = newNode;
+		}
+		else {
+		    current = this._head;
+		    while(current.next != null) {
+		        current = current.next;
+		    }
+		    current.next = newNode;
+		}
+		this._size++;
+	},
 	pop: function(){
 		var prev, current;
 		prev = this._head;
-        current = this._head.next;
+		current = this._head.next;
 		while(current.next != null) {
 			prev = current;
 			current = current.next;
@@ -41,32 +41,32 @@ LinkedList.prototype = {
 		var newNode = this._createNode(val);
 		newNode.next = this._head;
 		this._head = newNode;
-        this._size++;
+		this._size++;
 	},
 	unShift: function(val){
 		var prev, current;
 		prev = this._head;
 		current = this._head.next;
 		this._head = current;
-        this._size--;
+		this._size--;
 		return prev.data;
 	},
-    removeDuplicates: function() {
+	removeDuplicates: function() {
 		var prev, current;
-        prev = this._head;
-        current = this._head.next;
-        hash = {};
-        while(current != null) {
-            if(!hash[current.data]) {
-                hash[current.data] = true;
-            } else {
-                prev.next = current.next;
-                this._size--;
-            }
-            prev = current;
-            current = current.next;
-        }
-    },
+		prev = this._head;
+		current = this._head.next;
+		hash = {};
+		while(current != null) {
+		    if(!hash[current.data]) {
+		        hash[current.data] = true;
+		    } else {
+		        prev.next = current.next;
+		        this._size--;
+		    }
+		    prev = current;
+		    current = current.next;
+		}
+	},
 	removeSpecific: function(node){
 		var prev, current;
 		prev = this._head;
@@ -80,5 +80,17 @@ LinkedList.prototype = {
 				break;
 			}
 		}
+	},
+	reverse: function(){
+		var prev = null;
+		var current = this._head;
+		var next;
+		while(current){
+			next = current.next;
+			current.next = prev;
+			prev = current;
+			current = next;
+		}
+		this._head = prev;
 	}
 }
